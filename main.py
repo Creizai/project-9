a0673426fcad11f87ecf364baaca22c0495cbf5b
Adam_Bates <Creizai@EMAIL.COM>
2020-01-16 15:14:05 -0500

    added a menu for command line interface and added connecting code

--- main.py
+++ main.py
@@ -27,25 +27,22 @@ db.connect()
 # Create tables
 db.create_tables([Contact])
 
+
 def create_contact():
-    print("Enter the Contact's Name: ")
-    new_name = str(input())
-    print("Enter the Contact's Phone Number: ")
-    new_phone = str(input())
-    print("Enter the Contact's Address: ")
-    new_address = str(input())
-    print("Enter the Contact's description: ")
-    new_description = str(input())
+    new_name = input("Enter the Contact's Name: ")
+    new_phone = input("Enter the Contact's Phone Number: ")
+    new_address = input("Enter the Contact's Address: ")
+    new_description = input("Enter the Contact's description: ")
 
     new_contact = Contact(name=new_name, phone=new_phone, address=new_address, description=new_description)
     new_contact.save()
 
-adam = Contact(
-    name="Adam", 
-    phone=(5134179949), 
-    address="4802 Chief Chris Kyle Ct", 
-    description="Loud and annoying")
-adam.save()
+# adam = Contact(
+#     name="Adam", 
+#     phone=(5134179949), 
+#     address="4802 Chief Chris Kyle Ct", 
+#     description="Loud and annoying")
+# adam.save()
 # erin = Contact(name="Erin", phone=2025171777, address="509 7th St NW, Washington, DC 20004", birthday=date(1986, 1, 27), description="The Head SEI34 Instructor")
 # roger = Contact(name="Roger", phone=2025171777, address="509 7th St NW, Washington, DC 20004", birthday=date(1990, 5, 27), description="The Head SEI34 Instructor")
 # zakk.save()
@@ -85,23 +82,37 @@ def all_contacts():
 # zakk = Contact.get(Contact.name == 'Zakk')
 # zakk.birthday = date(1980, 11, 18)
 # zakk.save()
-def update_contact(name):
-    zakk = Contact.get(Contact.name == name)
-    zakk.birthday = date(1980, 11, 18)
-    zakk.save()
+def update_contact():
+    name = str(input("Enter the name of the contact you want to Delete: "))
+    contact = Contact.get(Contact.name == name)
+    contact.save()
 
 # Delete:
 # zakk.delete_instance()
+def delete_contact():
+    name = str(input("Enter the name of the contact you want to Delete: "))
+    contact = Contact.get(Contact.name == name)
+    contact.delete_instance()
 
 
 # print(f"{zakk.name} was born on {zakk.birthday}")
 # print(f"{erin.name} was born on {erin.birthday}")
 # print(f"{roger.name} was born on {roger.birthday}")
 
-
-all_contacts()
-print('Would you like to add a new contact? y / n')
-answer = str(input())
-if answer == 'y':
-    create_contact()
-all_contacts()
+navigation = 0
+while navigation != 5:
+    print('Contacts Book')
+    print('1. Create a new contact')
+    print('2. Read all contacts')
+    print('3. Update a contact')
+    print('4. Delete a contact')
+    print('5. Exit the Program')
+    navigation = int(input('Enter #:'))
+    if(navigation == 1):
+        create_contact()
+    elif(navigation == 2):
+        all_contacts()
+    elif(navigation == 3):
+        update_contact()
+    elif(navigation == 4):
+        delete_contact()
