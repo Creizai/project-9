3cce5fb63a2d80a9f2fc28f37b040005b8fc8175
Adam_Bates <Creizai@EMAIL.COM>
2020-01-16 15:39:41 -0500

    Added update logic

--- main.py
+++ main.py
@@ -82,16 +82,45 @@ def all_contacts():
 # zakk.birthday = date(1980, 11, 18)
 # zakk.save()
 def update_contact():
-    name = str(input("Enter the name of the contact you want to Delete: "))
+    name = str(input("Enter the name of the contact you want to Update: "))
     contact = Contact.get(Contact.name == name)
-    contact.save("\n Updated Contact! \n")
+    update = 0
+    while update != 5:
+        print('\nWhat Key would you like to update?\n')
+        print('1. Name')
+        print('2. Phone Number')
+        print('3. Address')
+        print('4. Description')
+        print('5. None! Get me outta here!)
+        update = int(input('Enter #:'))
+        if(update == 1):
+            print(f'Old Name: ' {contact.name})
+            contact.name = input("Enter New Name: ")
+            contact.save()
+            print('Name Changed!')
+        elif(update == 2):
+            print(f'Old Phone Number: ' {contact.phone})
+            contact.phone = input("Enter New Phone Number: ")
+            contact.save()
+            print('Phone Number Changed!')            
+        elif(update == 3):
+            print(f'Old Address: ' {contact.address})
+            contact.address = input("Enter New Address: ")
+            contact.save()
+            print('Address Changed!')            
+        elif(update == 4):
+            print(f'Old Description: ' {contact.description})
+            contact.description = input("Enter New Description: ")
+            contact.save()
+            print('Description Changed!')            
 
 # Delete:
 # zakk.delete_instance()
 def delete_contact():
     name = str(input("Enter the name of the contact you want to Delete: "))
     contact = Contact.get(Contact.name == name)
-    contact.delete_instance("\n Deleted Contact!\n")
+    contact.delete_instance()
+    print("\n Deleted Contact!\n")
 
 
 # print(f"{zakk.name} was born on {zakk.birthday}")
@@ -100,7 +129,7 @@ def delete_contact():
 
 navigation = 0
 while navigation != 5:
-    print('Contacts Book')
+    print('\nContacts Book\n')
     print('1. Create a new contact')
     print('2. Read all contacts')
     print('3. Update a contact')
